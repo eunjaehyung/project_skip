e3b5f1672632875d0341a64c00f4a3a96704a851
eunjaehyung <>
2019-04-07 01:32:59 +0900

    add parser data (test)

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string file = "/Resources/MasterTable/MasterTable";
+        
+        string dir = Application.dataPath + file + ".csv";
+        if (!File.Exists(dir))
+        {
+            Debug.Log(file + "파일이 존재하지않습니다.");
+        }
+        
+        string source;
+        StreamReader sr = new StreamReader(dir);
+        source = sr.ReadToEnd();
+        sr.Close();
+        List<Dictionary<string, object>> contain = CSVReader.Read(source);
+        
+        Debug.Log(contain);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
