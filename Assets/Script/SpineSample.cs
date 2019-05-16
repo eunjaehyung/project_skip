3e98b16ea8fff780fbb202e73fc8b0309b9887b6
shiho hishida <shiho_hishida@f4samurai.jp>
2019-05-16 22:08:20 +0900

    spine テスト

---
+++ Assets/Script/SpineSample.cs
@@ -0,0 +1,27 @@
+﻿using Spine.Unity;
+using UnityEngine;
+
+public class SpineSample : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject _targetSpine = null;     // 対象のSpineObject
+	[SerializeField]
+	private string _animationName = "";         // 再生するアニメーション名
+
+	// ボタンをクリック
+	public void OnClick()
+	{
+		if (_targetSpine == null || _animationName == "")
+		{
+			return;
+		}
+
+		// 指定のアニメーションを再生
+		SkeletonAnimation skeletonAnimation = _targetSpine.GetComponent<SkeletonAnimation>();
+		if (skeletonAnimation != null)
+		{
+			Spine.AnimationState animationState = skeletonAnimation.state;
+			animationState.SetAnimation(0, _animationName, true);
+		}
+	}
+}
