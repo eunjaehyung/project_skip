92dd805fa2f0663745584604d95d6effd8c05a8a
ToshikiHata <toshiki_hata@f4samurai.jp>
2019-05-17 10:05:42 +0900

    試しに吹き出しを動かして見るためのGameObjectとスクリプトを追加

---
+++ Assets/Script/FukidashiTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+[RequireComponent(typeof(SkeletonGraphic))]
+public class FukidashiTest : MonoBehaviour
+{
+    [SerializeField]
+    private string _animationName = "fukidashi";
+
+    void Start()
+    {
+        SkeletonGraphic skeletonGraphic = GetComponent<SkeletonGraphic>();
+        skeletonGraphic.AnimationState.SetAnimation(0, _animationName, true);
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("fukidashi was tapped.");
+    }
+}
