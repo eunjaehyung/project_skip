593a74b1ea54ac834478cc0789af861693aad20b
Eun JaeHyung <eun_jaehyung@f4samurai.jp>
2019-05-20 19:02:27 +0900

    no message

--- Assets/Script/CameraManScript.cs
+++ Assets/Script/CameraManScript.cs
@@ -1,7 +1,22 @@
+using UnityEngine;
+
 namespace Script
 {
-    public class CameraManScript
+    public class CameraManScript : MonoBehaviour
     {
-        
+        public Transform Target;
+
+        private void Update()
+        {
+            Vector3 dis = Target.transform.localPosition - transform.localPosition;
+
+            Vector3 vec = transform.localPosition;
+
+            vec.y = vec.y;
+
+            vec.x = dis.x / 16f;
+
+            transform.localPosition = vec;
+        }
     }
 }
