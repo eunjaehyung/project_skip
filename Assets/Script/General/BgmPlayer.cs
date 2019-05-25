903ec51f943cf9ba052687c4265a8f736f09f7af
ToshikiHata <toshiki_hata@f4samurai.jp>
2019-05-25 10:43:02 +0900

    ゲーム起動時に､BGMがループ再生するように ※シーン非依存

---
+++ Assets/Script/General/BgmPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ゲーム起動時にBGM再生を始めるオブジェクトを生成する.
+public class BgmPlayer : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod]
+    static private void Play()
+    {
+        var bgmPlayer = new GameObject("BgmPlayer");
+        DontDestroyOnLoad(bgmPlayer);
+
+        var audioSource = bgmPlayer.AddComponent<AudioSource>() as AudioSource;
+        audioSource.clip = Resources.Load<AudioClip>("Sound/Bg/bgm01") as AudioClip;
+        Debug.Assert(audioSource.clip != null);
+
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+}
