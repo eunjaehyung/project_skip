f65322b084110d6b36cacfa81cd48ff3ea9835b3
ToshikiHata <toshiki_hata@f4samurai.jp>
2019-05-25 09:48:54 +0900

    タイトルの難易度ボタンに関して調整
    ・ボタンOn・Offでテキストの色が切り替わるように
    ・アンカーポイントを調整
    ・テキストカラーを調整

---
+++ Assets/Script/SceneTitle/LevelButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class LevelButton : MonoBehaviour
+{
+    [SerializeField]
+    private Text _buttonLabel = null;
+
+    [SerializeField]
+    private string _htmlColorOn = "#00E4CA";
+
+    [SerializeField]
+    private string _htmlColorOff = "#404040";
+
+    private Color _colorOn;
+    private Color _colorOff;
+
+    void Start()
+    {
+        Debug.Assert(_buttonLabel != null);
+
+        bool parseResult = true;
+        parseResult = ColorUtility.TryParseHtmlString(_htmlColorOn,  out _colorOn)  && parseResult;
+        parseResult = ColorUtility.TryParseHtmlString(_htmlColorOff, out _colorOff) && parseResult;
+        Debug.Assert(parseResult);
+
+        var toggle = GetComponent<Toggle>();
+        toggle.onValueChanged.AddListener((bool isOn) => {
+            if (isOn) {
+                _buttonLabel.color = _colorOn;
+            } else {
+                _buttonLabel.color = _colorOff;
+            }
+        });
+    }
+}
