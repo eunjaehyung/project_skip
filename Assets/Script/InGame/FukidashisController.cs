a5aaf7567b45d06a82b336d8daa2211b1672f475
ToshikiHata <toshiki_hata@f4samurai.jp>
2019-05-26 20:43:26 +0900

    吹き出しシャッフル処理を追加

--- Assets/Script/InGame/FukidashisController.cs
+++ Assets/Script/InGame/FukidashisController.cs
@@ -15,6 +15,13 @@ public class FukidashisController : MonoBehaviour
     [SerializeField]
     private Canvas _canvas = null;
 
+    [SerializeField]
+    private Dictionary<int, float> _levelAndShuffleFreqTime = new Dictionary<int, float>(){
+        {1, 7.5f},
+        {2, 5.0f},
+        {3, 3.5f},
+    };
+
     private enum State {
         NotStart,  // 起動前.
         StartStep, // ステップ開始後.
@@ -22,7 +29,12 @@ public class FukidashisController : MonoBehaviour
         EndStep,   // ステップ終了.
         End,       // ゲーム終了.
     }
-    private State _state;
+    private State _state = State.NotStart;
+
+    private float _shuffleElapsedTime = 0.0f;
+    private float _shuffleFreqTime = 4.0f;
+    private float _hideElapsedTime = 0.0f;
+    private float _hideEndTime = 0.25f;
 
 
     // 現在のステップにおける､吹き出しオブジェクトのリスト.
@@ -32,21 +44,37 @@ public class FukidashisController : MonoBehaviour
     {
         Debug.Assert(_fukidashiPrefab != null);
         Debug.Assert(_canvas          != null);
+
+        _shuffleFreqTime = _levelAndShuffleFreqTime[LevelManager.Instance.Level];
     }
 
-    private void Start()
+    private void FixedUpdate()
     {
-        _state = State.NotStart;
-    }
+        if (_state == State.StartStep) {
+            _shuffleElapsedTime += Time.fixedDeltaTime;
+            if (_shuffleElapsedTime >= _shuffleFreqTime) {
+                _shuffleElapsedTime = 0.0f;
+                _state = State.Shuffling;
+                foreach (var fukidashi in _fukidashiList) { fukidashi.gameObject.SetActive(false); }
+                Shuffle();
+            }
+        }
 
-    private void Update()
-    {
-        
+        if (_state == State.Shuffling) {
+            _hideElapsedTime += Time.fixedDeltaTime;
+            if (_hideElapsedTime >= _hideEndTime) {
+                _hideElapsedTime = 0.0f;
+                _state = State.StartStep;
+                foreach (var fukidashi in _fukidashiList) { fukidashi.gameObject.SetActive(true); }
+            }
+        }
     }
 
     public void InstantiateObjects(int step, Action<bool> onTouch)
     {
         _state = State.StartStep;
+        _shuffleElapsedTime = 0.0f;
+        _hideElapsedTime = 0.0f;
 
         // 現在のステップの正解id.
         MasterItemStep stepMaster = MasterStepHolder.Instance().GetOneOrFail(LevelManager.Instance.Level, step);
@@ -105,5 +133,21 @@ public class FukidashisController : MonoBehaviour
             Destroy(fukidashi.gameObject);
         }
         _fukidashiList.Clear();
+    }
+
+    private void Shuffle()
+    {
+        int n = (int) Mathf.Ceil( _fukidashiList.Count / 2 );
+        for (int i = 0; i < n; i++) {
+            int i1 = UnityEngine.Random.Range(0, _fukidashiList.Count);
+            int i2 = UnityEngine.Random.Range(0, _fukidashiList.Count);
+            
+            var item1 = _fukidashiList[i1];
+            var item2 = _fukidashiList[i2];
+
+            Vector2 tmpPos = item1.GetComponent<RectTransform>().anchoredPosition;
+            item1.GetComponent<RectTransform>().anchoredPosition = item2.GetComponent<RectTransform>().anchoredPosition;
+            item2.GetComponent<RectTransform>().anchoredPosition = tmpPos;
+        }
     }
 }
